762715777ba6265ffffa3fd0c39ed6e2082c5221
miguel <miguemejia055@gmail.com>
2024-02-08 14:54:08 -0600

    archivos agregados

---
+++ app/lib/pantalla_principal.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+class PantallaPrincipal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Destinos Turísticos'),
+      ),
+      body: Container(
+        // Contenido de PantallaPrincipal
+      ),
+    );
+  }
+
+  void navigateToRegistro(BuildContext context) {
+    // Función _navigateToRegistro
+  }
+}
